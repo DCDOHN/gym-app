791bdd61793a928f67ec1bfcf783b5ab633be8e3
Hugh Nolan <hughnolan27@icloud.com>
2025-09-10 22:03:52 +0100

    Add add workout page and forms lots to do

--- app/workouts/page.tsx
+++ app/workouts/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/lib/supabase/server";
 import WorkoutBubble from "../_components/WorkoutBubble";
+import Link from "next/link";
 
 export default async function page() {
     const supabase = await createClient();
@@ -10,7 +11,7 @@ export default async function page() {
             <h1>Workouts</h1>
             <section>
                 <h2>Your workouts</h2>
-                <div>add workout</div>
+                <Link href="/AddWorkout">Add Workout</Link>
                 {workouts?.map((workout) => (
                     <WorkoutBubble key={workout.id} workoutData={workout} />
                 )) || <p>No workouts found.</p>}
