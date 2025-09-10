6a350d5cef5a6772de3094ccac504d4c5480b731
Hugh Nolan <hughnolan27@icloud.com>
2025-09-10 20:12:59 +0100

    Change to select workouts

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -2,7 +2,7 @@ import { createClient } from '@/lib/supabase/server'
 
 export default async function Page() {
   const supabase = await createClient()
-  const { data: notes } = await supabase.from('notes').select()
+  const { data: notes } = await supabase.from('workouts').select()
 
   return <pre>{JSON.stringify(notes, null, 2)}</pre>
 }
